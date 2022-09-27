95eed3af0498e9123e412c0e31fa33aec494dbc6
calvinhus <gjpcalvinho@gmail.com>
2022-09-27 19:03:39 +0100

    flask app

---
+++ lemon.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+from DFRobot_ADS1115 import ADS1115
+import os
+import sys
+import time
+from flask import Flask, render_template, request
+
+sys.path.append('../')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+soil_sensor = ADS1115()
+
+app = Flask(__name__)
+
+
+# home route
+@app.route('/')
+def index():
+
+    # Set the IIC address
+    soil_sensor.set_addr_ADS1115(0x48)
+    # Get the Digital Value of Analog of selected channel
+    soil_moisture = soil_sensor.read_voltage(0)
+
+    # variables to pass through to the web page
+    templateData = {
+        'title': 'Smart Garden',
+        'humidity': 75,
+        'temperature': 35,
+        'light': 125,
+        'temp': soil_moisture
+    }
+    # when a html request has been made return these values
+    return render_template('index.html', **templateData)
+
+
+if __name__ == '__main__':
+    #app.jinja_env.cache = {}
+    app.run(debug=True, port=5000)
