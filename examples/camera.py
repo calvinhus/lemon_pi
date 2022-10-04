85d0a0f7ffdfe897525873e3fb7e306e6e9e0222
calvinhus <gjpcalvinho@gmail.com>
2022-10-04 15:06:48 +0100

    updated camera script

--- examples/camera.py
+++ examples/camera.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 from picamera import PiCamera
+from datetime import datetime
+
+path = "/home/pi/lemon_pi/static/images/"
+timestamp = datetime.now().strftime("%d-%m-%Y_%Hh%Mm")
+full_path = path + timestamp + ".jpg"
 
 camera = PiCamera()
-camera.capture('/home/pi/lemon_pi/static/images/picture_test.jpg')
+camera.capture(full_path)
